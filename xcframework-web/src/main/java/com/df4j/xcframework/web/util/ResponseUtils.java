678371952db5f8443ded944f48a5432f8bd56cf7
王学成 <wangxuecheng@dgzq.com.cn>
2020-12-18 14:31:47 +0800

    写结果响应的工具类

--- xcframework-web/src/main/java/com/df4j/xcframework/web/util/ResponseUtils.java
+++ xcframework-web/src/main/java/com/df4j/xcframework/web/util/ResponseUtils.java
@@ -1,8 +1,11 @@
 package com.df4j.xcframework.web.util;
 
+import com.df4j.xcframework.base.server.Result;
+import com.df4j.xcframework.base.util.FileUtils;
 import com.df4j.xcframework.base.util.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
@@ -12,19 +15,20 @@ import java.io.Writer;
  */
 public class ResponseUtils {
     private static Logger logger = LoggerFactory.getLogger(ResponseUtils.class);
-    /*public static void returnJsonResult(HttpServletResponse response, Result result){
+
+    public static void returnJsonResult(HttpServletResponse response, Result result) {
         Writer writer = null;
         Exception exception = null;
         try {
             writer = response.getWriter();
-            response.setHeader("Content-Type","application/json;charset=UTF-8");
+            response.setHeader("Content-Type", "application/json;charset=UTF-8");
             writer.write(JsonUtils.stringify(result, true));
             writer.flush();
-        }catch (IOException e){
+        } catch (IOException e) {
             exception = e;
-            logger.error("将Result用Json格式写入response失败,result:",result);
-        }finally {
+            logger.error("将Result用Json格式写入response失败,result:", result);
+        } finally {
             FileUtils.close(writer, exception);
         }
-    }*/
+    }
 }
