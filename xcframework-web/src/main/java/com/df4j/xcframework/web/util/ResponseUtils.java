3f87e8e3b098690e883da11f2aa3d38ddc72de57
王学成 <wangxuecheng@dgzq.com.cn>
2020-12-18 10:16:12 +0800

    调整spring-boot的依赖

---
+++ xcframework-web/src/main/java/com/df4j/xcframework/web/util/ResponseUtils.java
@@ -0,0 +1,30 @@
+package com.df4j.xcframework.web.util;
+
+import com.df4j.xcframework.base.util.JsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * 响应工具类
+ */
+public class ResponseUtils {
+    private static Logger logger = LoggerFactory.getLogger(ResponseUtils.class);
+    /*public static void returnJsonResult(HttpServletResponse response, Result result){
+        Writer writer = null;
+        Exception exception = null;
+        try {
+            writer = response.getWriter();
+            response.setHeader("Content-Type","application/json;charset=UTF-8");
+            writer.write(JsonUtils.stringify(result, true));
+            writer.flush();
+        }catch (IOException e){
+            exception = e;
+            logger.error("将Result用Json格式写入response失败,result:",result);
+        }finally {
+            FileUtils.close(writer, exception);
+        }
+    }*/
+}
---
+++ xcframework-webflux/src/main/java/com/df4j/xcframework/webflux/None.java
@@ -0,0 +1,4 @@
+package com.df4j.xcframework.webflux;
+
+public class None {
+}
